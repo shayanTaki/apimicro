60d634ce83696caee0c0340951dea221c812d2ad
shayan taki <takishayan@gmail.com>
2024-12-31 02:41:51 +0330

    edit settings.py

--- apimicro/settings.py
+++ apimicro/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'appme.apps.AppmeConfig',
+    'rest_framework,'
 ]
 
 MIDDLEWARE = [
@@ -101,7 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
