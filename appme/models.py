9a39d66b7b6cfa7cffb7893d67ceb28175f21941
shayan taki <takishayan@gmail.com>
2024-12-31 02:43:27 +0330

    add model

--- appme/models.py
+++ appme/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+#مدل ذخیره ی یوزر جدید
+class RegistrationRequest(models.Model):
+    admin_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='registration_requests_made')
+    new_username = models.CharField(max_length=150, unique=True)
+    new_password = models.CharField(max_length=128)  # رمز عبور به صورت هش شده ذخیره می شود
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Request for {self.new_username} by {self.admin_user.username}"
